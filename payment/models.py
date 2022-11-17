5e09829edbd8281b53f4a70d367f2ed3fc3a7271
Igor Candido <igorcr39@gmail.com>
2022-11-17 08:00:15 -0300

    adjusted models

--- payment/models.py
+++ payment/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from decimal import Decimal
+from shopping.models import Compra
 
 class Pagamento(models.Model):
     compra_id = models.ForeignKey(
-        'compras.Compra', related_name="parcela_pagamento", on_delete=models.CASCADE)
+        Compra, related_name="parcela_pagamento", on_delete=models.CASCADE)
     indice_parcela = models.IntegerField()
     vencimento = models.DateField()
     valor_parcela = models.DecimalField(max_digits=13, decimal_places=2, validators=[
--- shopping/models.py
+++ shopping/models.py
@@ -1,27 +1,13 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from decimal import Decimal
+from products.models import Produto
 
 TIPOS_DESCONTO_ESCOLHAS = (
     (u'0', u'Valor'),
     (u'1', u'Percentual'),
 )
 
-class ItensCompra(models.Model):
-    produto = models.ForeignKey('cadastro.Produto', related_name="compra_produto",
-                                on_delete=models.CASCADE, null=True, blank=True)
-    compra_id = models.ForeignKey(
-        'compras.Compra', related_name="itens_compra", on_delete=models.CASCADE)
-    quantidade = models.DecimalField(max_digits=13, decimal_places=2, validators=[
-                                     MinValueValidator(Decimal('0.00'))], null=True, blank=True)
-    valor_unit = models.DecimalField(max_digits=13, decimal_places=2, validators=[
-                                     MinValueValidator(Decimal('0.00'))], null=True, blank=True)
-    tipo_desconto = models.CharField(
-        max_length=1, choices=TIPOS_DESCONTO_ESCOLHAS, null=True, blank=True)
-    desconto = models.DecimalField(max_digits=13, decimal_places=2, validators=[
-                                   MinValueValidator(Decimal('0.00'))], null=True, blank=True)
-    subtotal = models.DecimalField(max_digits=13, decimal_places=2, validators=[
-                                   MinValueValidator(Decimal('0.00'))], null=True, blank=True)
 
 class Compra(models.Model):
     # Info
@@ -37,4 +23,24 @@ class Compra(models.Model):
     seguro = models.DecimalField(max_digits=13, decimal_places=2, validators=[
                                  MinValueValidator(Decimal('0.00'))], default=Decimal('0.00'))
     observacoes = models.CharField(max_length=1055, null=True, blank=True)
+    
+class ItensCompra(models.Model):
+    produto = models.ForeignKey(Produto, related_name="compra_produto",
+                                on_delete=models.CASCADE, null=True, blank=True)
+    compra_id = models.ForeignKey(
+        Compra, related_name="itens_compra", on_delete=models.CASCADE)
+    quantidade = models.DecimalField(max_digits=13, decimal_places=2, validators=[
+                                     MinValueValidator(Decimal('0.00'))], null=True, blank=True)
+    valor_unit = models.DecimalField(max_digits=13, decimal_places=2, validators=[
+                                     MinValueValidator(Decimal('0.00'))], null=True, blank=True)
+    tipo_desconto = models.CharField(
+        max_length=1, choices=TIPOS_DESCONTO_ESCOLHAS, null=True, blank=True)
+    desconto = models.DecimalField(max_digits=13, decimal_places=2, validators=[
+                                   MinValueValidator(Decimal('0.00'))], null=True, blank=True)
+    subtotal = models.DecimalField(max_digits=13, decimal_places=2, validators=[
+                                   MinValueValidator(Decimal('0.00'))], null=True, blank=True)
+
+
+
+
 
